61f10ccddd52322cadff0752eea27a9bcb0740e1
c_mallik <c_mallik@qualcomm.com>
2009-05-18 16:13:32 +0530

    Music : Fix "CursorIndexOutOfBounds" exception when setting name title
    Added cursor object check prior to moving the cursor to the first row to
    prevent "CursorIndexOutOfBounds" exception when setting title.
    
    CRs-Fixed: 182653

--- src/com/android/music/AlbumBrowserActivity.java
+++ src/com/android/music/AlbumBrowserActivity.java
@@ -214,7 +214,10 @@ public class AlbumBrowserActivity extends ListActivity
 
     private void setTitle() {
         CharSequence fancyName = "";
-        if (mAlbumCursor != null && mAlbumCursor.getCount() > 0) {
+        //Added check on the cursor object. It shouldn't point to the position before
+        //first or after last row to prevent "CursorIndexOutOfBounds" exception.
+        if (mAlbumCursor != null && mAlbumCursor.getCount() > 0 &&
+            !mAlbumCursor.isBeforeFirst() && !mAlbumCursor.isAfterLast()) {
             mAlbumCursor.moveToFirst();
             fancyName = mAlbumCursor.getString(3);
             if (fancyName == null || fancyName.equals(MediaFile.UNKNOWN_STRING))
